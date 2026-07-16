83f815952cbedc511439a038b2f3a9fd0340d858
Avi-Gobrin <avichaigobrin@gmail.com>
2026-07-16 03:43:23 -0400

    Step 30: naive_softmax_1d

--- model.py
+++ model.py
@@ -239,8 +239,15 @@ def sum_keepdims(arr, axis):
     # TODO: sum along the given axis preserving the reduced dim as size 1
     return np.sum(arr, axis, keepdims=True)
 
-# Step 30 - naive_softmax_1d (not yet solved)
-# TODO: implement
+# Step 30 - naive_softmax_1d
+import numpy as np
+
+def naive_softmax_1d(logits):
+    """Compute softmax of a 1D logits vector via the direct exp/sum formula."""
+    # TODO: exponentiate the logits, then divide by their total sum
+    e = array_exp(logits)
+    e = e / sum_all(e)
+    return e
 
 # Step 31 - softmax_overflow_demo (not yet solved)
 # TODO: implement
