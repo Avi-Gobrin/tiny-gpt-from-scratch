bdebd427ce34b04d5450c4ab81a5c472d64c7024
Avi-Gobrin <avichaigobrin@gmail.com>
2026-08-09 18:21:04 +0000

    Step 126: transpose_heads_to_back

--- model.py
+++ model.py
@@ -939,8 +939,10 @@ def multihead_weighted_sum(weights, v):
     """Mix each head's value vectors using that head's attention weights."""
     return np.matmul(weights, v)
 
-# Step 126 - transpose_heads_to_back (not yet solved)
-# TODO: implement
+# Step 126 - transpose_heads_to_back
+def transpose_heads_to_back(x):
+    """(B, H, T, d_head) -> (B, T, H, d_head), the inverse of transpose_heads_to_front."""
+    return np.transpose(x, (0, 2, 1, 3))
 
 # Step 127 - get_multihead_output_sequence_length (not yet solved)
 # TODO: implement
