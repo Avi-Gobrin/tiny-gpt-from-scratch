65411bbb9384a6f323d638d896edee51355f3b4d
Avi-Gobrin <avichaigobrin@gmail.com>
2026-08-09 18:21:05 +0000

    Step 151: adam_update_second_moment

--- model.py
+++ model.py
@@ -1215,8 +1215,10 @@ def adam_update_first_moment(m, grad, beta1=0.9):
     """Running average of the gradient."""
     return beta1 * m + (1 - beta1) * grad
 
-# Step 151 - adam_update_second_moment (not yet solved)
-# TODO: implement
+# Step 151 - adam_update_second_moment
+def adam_update_second_moment(v, grad, beta2=0.999):
+    """Running average of the squared gradient."""
+    return beta2 * v + (1 - beta2) * grad ** 2
 
 # Step 152 - adam_bias_correction (not yet solved)
 # TODO: implement
